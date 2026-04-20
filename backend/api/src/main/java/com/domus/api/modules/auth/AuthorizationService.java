fcbdd38b52677caa5146245533e6f83fcf33af7a
Filipe Oliveira <filipejpo@gmail.com>
2026-04-20 16:02:50 -0300

    Feat/security jwt (#6)
    * feat: adiciona cadastro de igreja
    
    * feat: implementa infra de segurança + jwt

--- backend/api/src/main/java/com/domus/api/modules/auth/AuthorizationService.java
+++ backend/api/src/main/java/com/domus/api/modules/auth/AuthorizationService.java
@@ -1,4 +1,20 @@
 package com.domus.api.modules.auth;
 
-public class AuthorizationService {
+import com.domus.api.modules.usuario.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com e-mail: " + username));
+    }
 }

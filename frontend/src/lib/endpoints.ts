2c2f245989d5ebf5784936376314415a1eb2fde1
Filipe <josefilipe.dev@gmail.com>
2026-06-13 12:56:15 -0300

    feat(usuario) adicionando endpoint de registrar usuario

--- frontend/src/lib/endpoints.ts
+++ frontend/src/lib/endpoints.ts
@@ -2,5 +2,9 @@ export const Endpoints = {
     auth: {
         LOGIN: '/auth/login',
         REGISTER_IGREJA: '/igrejas/registrar',
+    },
+
+    usuarios: {
+        REGISTRAR_USUARIO: '/usuarios/registrar',
     }
 }
